636e4bce3add923b436c8a4b579181eea1f97947
Cao Jiying <caojiying002@126.com>
2024-09-29 16:10:20 +0800

    图片加载使用单独的OkHttpClient

--- app/src/main/java/com/jiyingcao/a51fengliu/api/RetrofitClient.kt
+++ app/src/main/java/com/jiyingcao/a51fengliu/api/RetrofitClient.kt
@@ -1,6 +1,5 @@
 package com.jiyingcao.a51fengliu.api
 
-import com.jiyingcao.a51fengliu.glide.BASE_IMAGE_URL
 import java.util.concurrent.TimeUnit.SECONDS
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -11,13 +10,9 @@ private val DEBUG_HTTP: Boolean = (false)
 
 const val BASE_URL = "https://309.16dress.xyz/" //"https://903.16duty.xyz/"
 
-private const val USER_AGENT = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36"
-// User-Agent on Firefox, Windows
-// "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 object RetrofitClient {
 
-    val okHttpClient by lazy {
+    private val okHttpClient by lazy {
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = when (DEBUG_HTTP) {
@@ -25,19 +20,6 @@ object RetrofitClient {
                     false -> HttpLoggingInterceptor.Level.HEADERS
                 }
             })
-            // 添加拦截器增加两个图片专用请求头
-            .addInterceptor { chain ->
-                val originalRequest = chain.request()
-                // 如果URL是图片请求（主URL为BASE_IMAGE_URL），则添加两个图片专用请求头
-                if (originalRequest.url.toString().startsWith(BASE_IMAGE_URL)) {
-                    val requestWithHeaders = originalRequest.newBuilder()
-                        .header("Referer", BASE_URL)
-                        .header("User-Agent", USER_AGENT)
-                        .build()
-                    return@addInterceptor chain.proceed(requestWithHeaders)
-                }
-                return@addInterceptor chain.proceed(originalRequest)
-            }
             .connectTimeout(15, SECONDS)
             .readTimeout(15, SECONDS)
             .writeTimeout(15, SECONDS)
--- app/src/main/java/com/jiyingcao/a51fengliu/glide/OkHttpGlideModule.kt
+++ app/src/main/java/com/jiyingcao/a51fengliu/glide/OkHttpGlideModule.kt
@@ -7,15 +7,48 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
-import com.jiyingcao.a51fengliu.api.RetrofitClient
+import com.jiyingcao.a51fengliu.api.BASE_URL
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import java.io.InputStream
+import java.util.concurrent.TimeUnit.SECONDS
+
+private const val DEBUG_IMAGE_LOADING = false
+
+private const val USER_AGENT = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36"
+// User-Agent on Firefox, Windows
+// "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 @GlideModule
 class OkHttpGlideModule: AppGlideModule() {
+
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = when (DEBUG_IMAGE_LOADING) {
+                    true -> HttpLoggingInterceptor.Level.HEADERS
+                    false -> HttpLoggingInterceptor.Level.NONE
+                }
+            })
+            // 添加拦截器增加两个图片专用请求头
+            .addInterceptor { chain ->
+                // 如果URL是图片请求（主URL为BASE_IMAGE_URL），则添加两个图片专用请求头
+                // if (originalRequest.url.toString().startsWith(BASE_IMAGE_URL))
+                val originalRequest = chain.request()
+                val requestWithHeaders = originalRequest.newBuilder()
+                    .header("Referer", BASE_URL)
+                    .header("User-Agent", USER_AGENT)
+                    .build()
+                return@addInterceptor chain.proceed(requestWithHeaders)
+            }
+            .connectTimeout(10, SECONDS)
+            .readTimeout(10, SECONDS)
+            .writeTimeout(10, SECONDS)
+            .build()
+
+    }
+
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
-        // 使用Retrofit用的 OkHttpClient
-        val okHttpClient: OkHttpClient = RetrofitClient.okHttpClient
         registry.replace(
             GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(okHttpClient)
         )

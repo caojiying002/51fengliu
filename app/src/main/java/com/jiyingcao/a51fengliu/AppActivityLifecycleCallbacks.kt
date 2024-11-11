fea1853a6cb511000b80985cb79ce35c37710f46
Cao Jiying <caojiying002@126.com>
2024-11-11 14:09:51 +0800

    Rename `system_[top|bottom]_bar_padding` to `system_[top|bottom]_bar_space`

--- app/src/main/java/com/jiyingcao/a51fengliu/AppActivityLifecycleCallbacks.kt
+++ app/src/main/java/com/jiyingcao/a51fengliu/AppActivityLifecycleCallbacks.kt
@@ -42,9 +42,9 @@ object EdgeToEdgeWindowInsetsCallbacks : DefaultActivityLifecycleCallbacks() {
     private const val ANDROID_R_ID_CONTENT = android.R.id.content
 
     /** 顶部Space控件ID，高度设置为系统状态栏高度 */
-    private val SPACE_ID_SYSTEM_TOP_BAR_PADDING = R.id.system_top_bar_padding
+    private val SYSTEM_TOP_BAR_SPACE_ID = R.id.system_top_bar_space
     /** 底部Space控件ID，高度设置为系统导航条高度 */
-    private val SPACE_ID_SYSTEM_BOTTOM_BAR_PADDING = R.id.system_bottom_bar_padding
+    private val SYSTEM_BOTTOM_BAR_SPACE_ID = R.id.system_bottom_bar_space
 
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
         activity.apply {
@@ -60,10 +60,10 @@ object EdgeToEdgeWindowInsetsCallbacks : DefaultActivityLifecycleCallbacks() {
             ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
                 val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 
-                // 把[R.id.system_top_bar_padding]的高度设置为状态栏高度
-                v.findViewById<View>(SPACE_ID_SYSTEM_TOP_BAR_PADDING)?.updateLayoutParams { height = systemBarsInsets.top }
-                // 把[R.id.system_bottom_bar_padding]的高度设置为底部导航栏高度
-                v.findViewById<View>(SPACE_ID_SYSTEM_BOTTOM_BAR_PADDING)?.updateLayoutParams { height = systemBarsInsets.bottom }
+                // 把[R.id.system_top_bar_space]的高度设置为状态栏高度
+                v.findViewById<View>(SYSTEM_TOP_BAR_SPACE_ID)?.updateLayoutParams { height = systemBarsInsets.top }
+                // 把[R.id.system_bottom_bar_space]的高度设置为底部导航栏高度
+                v.findViewById<View>(SYSTEM_BOTTOM_BAR_SPACE_ID)?.updateLayoutParams { height = systemBarsInsets.bottom }
 
                 ViewCompat.setOnApplyWindowInsetsListener(v, null)
                 insets

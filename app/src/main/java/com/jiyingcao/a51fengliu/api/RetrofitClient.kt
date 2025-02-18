f0d996eb6ca0da720d9eb20c6406decd2ebe3d2c
Cao Jiying <caojiying002@126.com>
2025-02-18 20:52:20 +0800

    新的`BASE_URL`: 127can.info

--- app/src/main/java/com/jiyingcao/a51fengliu/api/RetrofitClient.kt
+++ app/src/main/java/com/jiyingcao/a51fengliu/api/RetrofitClient.kt
@@ -13,7 +13,7 @@ private val DEBUG_HTTP: Boolean = (true)
 private val USE_DEBUG_TOKEN: Boolean = (false)
 private val DEBUG_TOKEN: String = ""
 
-const val BASE_URL = "https://910.da576.xyz/"   //"https://309.16dress.xyz/" //"https://903.16duty.xyz/"
+const val BASE_URL = "https://127can.info/" //"https://910.da576.xyz/"   //"https://309.16dress.xyz/"
 
 object RetrofitClient {
 

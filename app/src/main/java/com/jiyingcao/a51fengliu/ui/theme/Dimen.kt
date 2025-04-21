97e2f6596ae036d8c5183d254acd00cc023f2ac0
Cao Jiying <caojiying002@126.com>
2025-04-21 10:26:59 +0800

    feat (Compose): Dimen.kt

---
+++ app/src/main/java/com/jiyingcao/a51fengliu/ui/theme/Dimen.kt
@@ -0,0 +1,29 @@
+package com.jiyingcao.a51fengliu.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+// Toolbar
+val ToolbarHeight = 56.dp
+
+// 页面默认的边距，也就是每个页面内容区域到屏幕边缘的距离
+val DefaultHorizontalSpace = 8.dp
+
+// Text sizes
+val TextDefaultSize = 14.dp
+val ButtonTextDefaultSize = 15.dp
+
+// Home screen specific
+val HomeIconSize = 32.dp
+val HomeIconDrawablePadding = 8.dp
+val HomeIconVerticalPadding = 8.dp
+
+// Divider
+val DividerHeight = 4.dp
+val DividerHeightHalf = 2.dp
+
+// Common spacings
+val spacingSmall = 4.dp
+val spacingMedium = 8.dp
+val spacingLarge = 16.dp
+val spacingXLarge = 24.dp
